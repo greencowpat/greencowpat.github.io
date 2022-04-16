83dff159b23ff3710ab39f72d4f76674204058fd
greencowpat <father_harris@hotmail.com>
2022-04-16 18:15:06 +1000

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+document.querySelector('button').addEventListener('click', getCocktail)
+
+const drinkArray = [];
+
+function getCocktail() {
+    let drink = document.querySelector('input').value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => {
+    return res.json()
+    })
+    .then(data => {
+
+        let canvas = document.querySelector('.canvas');
+        drinkArray.length = 0;
+        drinkArray.push(data.drinks)
+        let generatedHtml = data.drinks.reduce((accum, currKey, currIndex) => accum + 
+        `<div class="card" data-drink-id=${currKey.idDrink} data-index=${currIndex}>
+            <div class="thumbnail"><img src="${currKey.strDrinkThumb}"></div>
+            <div class="drink-name">${currKey.strDrink}</div>
+            <div class="base">${currKey.strIngredient1}</div>
+        </div>`,"")
+        canvas.innerHTML = generatedHtml;
+        console.log('success', data.drinks)
+    })
+    .catch(error => {
+    console.log('failed:', error)
+    })
+};
+
+    
+document.addEventListener('click', (e) => {
+    if(e.target.closest('.card')) {
+        let id = e.target.closest('.card');
+        console.log(id.dataset.index)
+    }
+})
